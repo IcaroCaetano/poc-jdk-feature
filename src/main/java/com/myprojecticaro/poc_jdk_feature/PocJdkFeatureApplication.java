3952560f06e4b07e7eb7b0fb67dbcda9f083d031
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-14 09:25:54 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_jdk_feature/PocJdkFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_jdk_feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJdkFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJdkFeatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_jdk_feature/PocJdkFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_jdk_feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJdkFeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
