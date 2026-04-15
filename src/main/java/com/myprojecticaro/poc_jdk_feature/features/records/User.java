105e0e15eae44a4e1a9ca451beef90ea77111702
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-15 09:08:38 -0300

    feat: add record constructor

--- src/main/java/com/myprojecticaro/poc_jdk_feature/features/records/User.java
+++ src/main/java/com/myprojecticaro/poc_jdk_feature/features/records/User.java
@@ -1,3 +1,10 @@
 package com.myprojecticaro.poc_jdk_feature.features.records;
 
-public record User(String name, int age) {}
+public record User(String name, int age) {
+
+    public User {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+    }
+}
